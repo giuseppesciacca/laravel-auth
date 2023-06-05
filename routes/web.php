40fcfac44e0caafd9065f1e3cc86a2f684954a48
giuseppesciacca <sciaccagi@gmail.com>
2023-06-05 16:21:46 +0200

    [add] admin.blade view + create db

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/* Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard'); */
-
-Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-});
+Route::middleware(['auth', 'verified'])
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])
+            ->name('dashboard');
+    });
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
